4bf0b17d2d61e3aa2204a24d6bc7f10176a5e72a
Sergii <s.ignatov92@gmail.com>
2026-03-04 21:45:06 +0200

    Initial Commit

---
+++ Subjecta-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Subjecta-App
+//
+//  Created by Sergii Ignatov on 04.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Subjecta-App/Subjecta_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Subjecta_AppApp.swift
+//  Subjecta-App
+//
+//  Created by Sergii Ignatov on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Subjecta_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
